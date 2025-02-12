258749c5c0cfd29637ab31460a033ccd90f207c9
akicool <liid05754@gmail.com>
2025-02-12 11:59:12 +0300

    feat: add api

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fastify_1 = require("fastify");
-var fastify = (0, fastify_1.default)({
-    logger: true,
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
 });
+
 fastify.get("/123", function (request, reply) {
-    reply.send({ hello: "world" });
+  reply.send({ hello: "world" });
 });
+
 fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 });
